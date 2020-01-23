b2b8284492e245e5a89c9e5c756f822e5631522e
Mlipphardt <matthewlipphardt@gmail.com>
2020-01-22 20:22:14 -0500

    For loop now generates initial buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,12 @@
-let martialarts = ["tai chi", "karata", "taekwondo"];
+let countries = ["america", "india", "china", "brazil"];
 let apikey = "HyrYFqZOH6QDHtwRJPcs925lbeU6l5z8";
 
 function buttonCreator(array){
     for(let i = 0; i < array.length; i++){
-        
+        let newButton = $("<button>");
+        $(newButton).text(array[i]);
+        $("#buttons").prepend(newButton);
     }
 }
+
+buttonCreator(countries);
