a4620037e81b2718410deb4db4fcc58eed630159
Mlipphardt <matthewlipphardt@gmail.com>
2020-01-22 20:14:02 -0500

    Initial commit, linked app.js to index.html, added jquery link

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+let martialarts = ["tai chi", "karata", "taekwondo"];
+let apikey = "HyrYFqZOH6QDHtwRJPcs925lbeU6l5z8";
+
+function buttonCreator(array){
+    for(let i = 0; i < array.length; i++){
+        
+    }
+}
